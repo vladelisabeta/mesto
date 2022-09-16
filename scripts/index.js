9131f797d77f7b825076736fb6c0bf91d0689ce1
Elisaveta Vlad <vladelisabeta@yandex.ru>
2022-09-16 21:16:37 +0300

    хочу деплой

--- scripts/index.js
+++ scripts/index.js
@@ -3,24 +3,30 @@ let edit = document.querySelector('.profile__edit-button');
 let buttonClose = popup.querySelector('.popup__button-close');
 let save = popup.querySelector('.popup__save');
 
-function openClosePopup() {
-  popup.classList.toggle('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
 }
 
 edit.addEventListener('click', {
-  handleEvent: (openClosePopup)
+  handleEvent: (openPopup)
 })
 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
 buttonClose.addEventListener('click', {
-  handleEvent: (openClosePopup)
+  handleEvent: (closePopup)
 })
+
+// INPUT
 
 let formElement = document.querySelector('.popup__form');
 
 // let nameInput = popup.querySelector('.popup__name');
 // let jobInput = popup.querySelector('.popup__info');
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
 
   let nameInput = popup.querySelector('.popup__name');
