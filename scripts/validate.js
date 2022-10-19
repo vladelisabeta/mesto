6f14b14536ab448ad57bc6a33745a594abe7c801
Elisaveta Vlad <vladelisabeta@yandex.ru>
2022-10-19 16:29:48 +0300

    подготовить все к началу скрипта

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,14 @@
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
 
+// ПРОПИСАТЬ СТИЛИ СПАНА ОШИБКИ
+// временный блок
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save',
-  inactiveButtonClass: 'popup__save_disabled', //save disabled сделать
-  inputErrorClass: 'popup__input_type_error', // сделать
-  errorClass: 'popup__error_visible' // сделать
+  inactiveButtonClass: 'popup__save_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible' // сделан. существует как класс. надо стилизовать и прописать везде.
 });
+
