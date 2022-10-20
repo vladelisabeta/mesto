ff2454813e4449612d8b9864bfb9ad03acd95efd
Elisaveta Vlad <vladelisabeta@yandex.ru>
2022-10-20 23:49:58 +0300

    я сделала что-то полезное

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,116 +1,56 @@
-const formPart = document.querySelector('.popup__form');
-const formInput = formPart.querySelector('.popup__input');
+const validateObj = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save',
+  inactiveButtonClass: 'popup__save_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
 
-function showInputError(element) {
-  element.classList.add('popup__input_type_error');
-};
-
-function hideInputError(element) {
-  element.classList.remove('popup__input_type_error');
-};
-
-function checkValidity() {
-  if (!formInput.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formInput);
-  } else {
-    // Если проходит, скроем
-    hideInputError(formInput);
-  }
-};
-
-formInput.addEventListener('input', checkValidity);
-
-
-
-
-
-
-
-
-
-
-// // включение валидации вызовом enableValidation
-// // все настройки передаются при вызове
-
-// // ПРОПИСАТЬ СТИЛИ СПАНА ОШИБКИ
-
-// // временный блок
-// // enableValidation({
-// //   formSelector: '.popup__form',
-// //   inputSelector: '.popup__input',
-// //   submitButtonSelector: '.popup__save',
-// //   inactiveButtonClass: 'popup__save_disabled',
-// //   inputErrorClass: 'popup__input_type_error',
-// //   errorClass: 'popup__error_visible' // сделан. существует как класс. надо стилизовать и прописать везде.
-// // });
-
-// // ФУНКЦИИ
-
-// // функция показа ошибки
-
-// function showInputError(formSelector, inputSelector, errorMessage) {
-//   const spanErrorMessage = formSelector.querySelector(`.${inputSelector.id}-error`); //errorElement
-//   inputSelector.classList.add('popup__input_type_error'); //ошибка ИНПУТА
-//   spanErrorMessage.textContent = errorMessage;
-//   spanErrorMessage.classList.add('popup__error_visible'); // текст ошибки
-// };
-// // функция сокрытия ошибки
-
-// function hideInputError(formSelector, inputSelector) {
-//   const spanErrorMessage = formSelector.querySelector(`.${inputSelector.id}-error`);
-//   inputSelector.classList.remove('popup__input_type_error');
-//   spanErrorMessage.classList.remove('popup__error_visible');
-//   spanErrorMessage.textContent = '';
-// };
-
-// // проверка валидности
-// function checkInputValidation(formSelector, inputSelector) {
-//   if (!inputSelector.vaidity.valid) {
-//     showInputError(formSelector, inputSelector, inputSelector.validationMessage);
-//   } else {
-//     hideInputError(formSelector, inputSelector);
-//   }
-// };
-
-// // функция создания обработчиков
-
-// function setListenersForInputs(formSelector) {
-//   const allInputs = Array.from(formSelector.querySelectorAll('.popup__input'));
-//   allInputs.forEach(createEventListener(inputSelector));
-// }
-
-// // функция создания ивент листенера
-// function createEventListener(inputSelector) {
-//   inputSelector.addEventListener('input', function () {
-//     checkInputValidation(formSelector, inputSelector);
-//   });
-// }
-
-// function enableValidation({
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__save',
-//   inactiveButtonClass: 'popup__save_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible'
-// }) {
+// function enableValidation({ formSelector, }) {
 //   const allForms = Array.from(document.querySelectorAll(formSelector));
-//   allForms.forEach(function (formSelector) {
-//     formSelector.addEventListener('submit', (evt) => {
-//       evt.preventDefault();
-//     });
-//     setListenersForInputs(formSelector);
-//   });
+//   console.log(allForms); // выводит две формы
+//   allForms.forEach(setEventListeners());
 // }
 
+// function setEventListeners(formObject) {
+//   const allInputs = Array.from(formObject.querySelectorAll(''))
+// } //скорее всего ненужная функция
 
-// // попытка вызвать функцию ._. не работает конечно же
-// enableValidation(({
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__save',
-//   inactiveButtonClass: 'popup__save_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible'
-// }))
+//функция установки событий на все инпуты в форме. вызывает функцию
+function setInputEventListeners(formObject) {
+  const allInputs = Array.from(formObject.querySelectorAll('.popup__input'));
+  console.log(allInputs); //выводит два инпута
+  allInputs.forEach(addEventListener('input', ));// здесь должен быть колбек проверки валидности и прочие приколы
+}
+
+
+
+
+// событие ввода текста
+
+function createEventListener(inputField) {
+  inputField.addEventListener('input', () => {
+    // checkCurrentInputValidity КОНКРЕТНОГО ЭТОГО ИНПУТА(invalid = > error*function*; valid => hide error*function*
+    // checkAllInputValidity (if invalid => disable save button) НУЖНА ФУНКЦИЯ ПРОВЕРКИ ВСЕЙ ФОРМЫ
+  })
+}
+
+function showInputError() {
+  //здесь может быть ваша реклама
+}
+
+
+function hideInputError() {
+  //здесь тоже может быть ваша реклама
+}
+
+function checkCurrentInputValidity() {
+// функция проверки конкретного инпута
+//(invalid = > error*function*;
+//valid => hide error*function*
+}
+
+function checkAllInputValidity() {
+  //разобраться что делает и кнопка!!!!!!!!!
+}
