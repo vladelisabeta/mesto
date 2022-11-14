41d1541a31827f1eefd989a340b78282578c26ef
Elisaveta Vlad <vladelisabeta@yandex.ru>
2022-11-15 00:07:43 +0300

    mama otboy

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -5,7 +5,7 @@ export class FormValidator {
   }
 
   enableValidation() {
-    this._formElement.this._setInputEventListeners();
+    this._setInputEventListeners();
   }
 
   _setInputEventListeners() {
@@ -21,63 +21,63 @@ export class FormValidator {
   _createEventListener(formInput, allInputs, formSaveButton) {
     formInput.addEventListener('input', () => {
       this._hasInvalidInput(formInput);
-      this._checkAllInputValidity(validateObj, allInputs, formSaveButton);
+      this._checkAllInputValidity(allInputs, formSaveButton);
     })
   }
 
 
- _checkAllInputValidity(allInputs, formSaveButton) {
-  if (this._reactIfHasInvalidInput(allInputs)) {
-    this._disableButtonSave(formSaveButton);
-  } else {
-    this._enableButtonSave(formSaveButton);
+  _checkAllInputValidity(allInputs, formSaveButton) {
+    if (this._reactIfHasInvalidInput(allInputs)) {
+      this._disableButtonSave(formSaveButton);
+    } else {
+      this._enableButtonSave(formSaveButton);
+    }
   }
-}
 
-_reactIfHasInvalidInput(allInputs) {
-  return allInputs.some((inputElement) => {
-    return !inputElement.validity.valid
-  })
-}
-
- _disableButtonSave(formSaveButton) {
-  formSaveButton.setAttribute('disabled', true);
-  formSaveButton.classList.add(this._validateObj.inactiveButtonClass);
-}
-
-_enableButtonSave(formSaveButton) {
-  formSaveButton.removeAttribute('disabled');
-  formSaveButton.classList.remove(this._validateObj.inactiveButtonClass);
-}
-
-
- _hasInvalidInput(formInput) {
-  if (!formInput.validity.valid) {
-    this._showInputError(formInput, formInput.validationMessage);
-  } else {
-    this._hideInputError(formInput);
+  _reactIfHasInvalidInput(allInputs) {
+    return allInputs.some((inputElement) => {
+      return !inputElement.validity.valid
+    })
   }
-}
 
-_showInputError(formInput, errorMessage) {
+  _disableButtonSave(formSaveButton) {
+    formSaveButton.setAttribute('disabled', true);
+    formSaveButton.classList.add(this._validateObj.inactiveButtonClass);
+  }
 
-  const span = `#${formInput.id}-error`; // #name-error
+  _enableButtonSave(formSaveButton) {
+    formSaveButton.removeAttribute('disabled');
+    formSaveButton.classList.remove(this._validateObj.inactiveButtonClass);
+  }
 
-  const spanErrorElement = this._formElement.querySelector(span);
 
-  formInput.classList.add(this._validateObj.inputErrorClass);
-  spanErrorElement.textContent = errorMessage;
-  spanErrorElement.classList.add(this._validateObj.errorClass);
-}
+  _hasInvalidInput(formInput) {
+    if (!formInput.validity.valid) {
+      this._showInputError(formInput, formInput.validationMessage);
+    } else {
+      this._hideInputError(formInput);
+    }
+  }
 
-_hideInputError(formInput) {
-  const span = `#${formInput.id}-error`;
-  const spanErrorElement = this._formElement.querySelector(span);
+  _showInputError(formInput, errorMessage) {
 
-  formInput.classList.remove(this._validateObj.inputErrorClass);
-  spanErrorElement.classList.remove(this._validateObj.errorClass);
-  spanErrorElement.textContent = '';
-}
+    const span = `#${formInput.id}-error`; // #name-error
+
+    const spanErrorElement = this._formElement.querySelector(span);
+
+    formInput.classList.add(this._validateObj.inputErrorClass);
+    spanErrorElement.textContent = errorMessage;
+    spanErrorElement.classList.add(this._validateObj.errorClass);
+  }
+
+  _hideInputError(formInput) {
+    const span = `#${formInput.id}-error`;
+    const spanErrorElement = this._formElement.querySelector(span);
+
+    formInput.classList.remove(this._validateObj.inputErrorClass);
+    spanErrorElement.classList.remove(this._validateObj.errorClass);
+    spanErrorElement.textContent = '';
+  }
 
 
 }
